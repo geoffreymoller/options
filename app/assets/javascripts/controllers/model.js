3698876bf1767a17a6ecf0c02607d338c147445b
Geoff Moller <geoff.moller@gmail.com>
2013-01-05 20:33:03 -0800

    default values

--- app/assets/javascripts/controllers/model.js
+++ app/assets/javascripts/controllers/model.js
@@ -1,5 +1,12 @@
 var ModelController = function($scope, $rootScope, $http, $location, $routeParams, $blackscholes) {
 
+  //default values
+  $scope.strike = 50;
+  $scope.underlying = 55;
+  $scope.tte = .08219; //30;
+  $scope.ir = .08; //8;
+  $scope.volatility = .30; //30;
+
   $scope.calc = function(){
     $scope.theoreticalCall = $blackscholes.calculate('c', parseFloat($scope.strike), parseFloat($scope.underlying), 
       parseFloat($scope.tte), parseFloat($scope.ir), parseFloat($scope.volatility));    
